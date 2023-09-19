a76ca359767fe6432ecc4c01273eed5329ed527c
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2023-09-18 22:15:22 -0700

    Upd

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,8 @@ import type { Metadata } from "next";
 import { SITE } from "~/config.js";
 
 import Hero from "~/components/widgets/Hero";
-import SocialProof from "../src/components/widgets/SocialProof";
-import Features3 from "~/components/widgets/Features3";
 import Content from "~/components/widgets/Content";
 import Steps from "~/components/widgets/Steps";
-import Testimonial from "~/components/widgets/Testimonial";
-import FAQs2 from "~/components/widgets/FAQs2";
-import Pricing from "~/components/widgets/Pricing";
-import Team from "~/components/widgets/Team";
-import CallToAction2 from "~/components/widgets/CallToAction2";
 import {
   callToActionData,
   // content2Data,
@@ -22,7 +15,6 @@ import {
   // pricingData,
   // socialProofData,
 } from "~/shared/data";
-import Contact from "~/components/widgets/Contact";
 
 export const metadata: Metadata = {
   title: SITE.title,
